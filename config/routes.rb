ccc00ab6ade4836ae0318aeae8f837f5e0bae635
hernandez87v <hernandez87v@hotmail.com>
2020-04-28 12:24:53 -0700

    Created route for new article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+ 
+  resources :articles
+ 
+  root 'welcome#index'
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
- root 'welcome#index'
+#  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,4 +59,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
